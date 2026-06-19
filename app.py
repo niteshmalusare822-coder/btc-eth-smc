d61e67f55c5f452d454c1809ae26dc9e917731f5
niteshmalusare822-coder <niteshmalusare822@gmail.com>
2026-06-20 04:20:23 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+from scanner import analyze
+
+app = Flask(__name__) # Yahan '__name__' use hota hai
+
+CORS(app)
+
+@app.route("/")
+def home():
+    return jsonify({
+        "status": "running"
+    })
+
+@app.route("/api/dashboard")
+def dashboard():
+    return jsonify({
+        "btc": {
+            "1m": analyze("BTC/USDT:USDT", "1m"),
+            "5m": analyze("BTC/USDT:USDT", "5m"),
+            "15m": analyze("BTC/USDT:USDT", "15m"),
+            "1h": analyze("BTC/USDT:USDT", "1h")
+        },
+        "eth": {
+            "1m": analyze("ETH/USDT:USDT", "1m"),
+            "5m": analyze("ETH/USDT:USDT", "5m"),
+            "15m": analyze("ETH/USDT:USDT", "15m"),
+            "1h": analyze("ETH/USDT:USDT", "1h")
+        }
+    })
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
