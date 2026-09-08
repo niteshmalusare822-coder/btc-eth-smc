a229231b638c02a3592405ad88f0b35b85232c2e
niteshmalusare822-coder <niteshmalusare822@gmail.com>
2026-09-09 01:22:16 +0530

    Update test_ws.py

--- test_ws.py
+++ test_ws.py
@@ -2,26 +2,19 @@ import socketio
 
 sio = socketio.Client(logger=True, engineio_logger=False)
 
-CHANNELS = [
-    "B-BTC_USDT_1m-futures",
-    "B-BTC_USDT_1h-futures",
-]
-
 @sio.event
 def connect():
     print(">>> connected, joining channels", flush=True)
-    for ch in CHANNELS:
-        sio.emit("join", {"channelName": ch})
-        print(f">>> joined {ch}", flush=True)
+    sio.emit('join', {'channelName': 'B-BTC_USDT_1m-futures'})
+    sio.emit('join', {'channelName': 'B-BTC_USDT@trades-futures'})
 
-@sio.on("candlestick")
-def candlestick(data):
-    print(f">>> CANDLESTICK: {data}", flush=True)
+@sio.on('candlestick')
+def on_candle(response):
+    print(f">>> CANDLESTICK: {response}", flush=True)
 
-@sio.on("*")
-def catch_all(event, data):
-    print(f">>> EVENT NAME: {event}", flush=True)
-    print(f">>> DATA: {data}", flush=True)
+@sio.on('new-trade')
+def on_trade(response):
+    print(f">>> TRADE: {response}", flush=True)
 
 sio.connect("wss://stream.coindcx.com", transports=["websocket"])
 sio.wait()
