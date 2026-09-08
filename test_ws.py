da295b6b21308d073447b07c1b261c6cc08f2f27
niteshmalusare822-coder <niteshmalusare822@gmail.com>
2026-09-09 01:03:07 +0530

    Update test_ws.py

--- test_ws.py
+++ test_ws.py
@@ -2,10 +2,19 @@ import socketio
 
 sio = socketio.Client(logger=True, engineio_logger=False)
 
+CHANNELS = [
+    "B-BTC_USDT@trades",
+    "B-BTC_USDT@prices",
+    "candlestick@B-BTC_USDT_1m",
+    "candlestick@B-BTC_USDT_1",
+]
+
 @sio.event
 def connect():
-    print(">>> connected, joining channel", flush=True)
-    sio.emit("join", "candlestick@B-BTC_USDT_1m")
+    print(">>> connected, joining channels", flush=True)
+    for ch in CHANNELS:
+        sio.emit("join", ch)
+        print(f">>> joined {ch}", flush=True)
 
 @sio.on("*")
 def catch_all(event, data):
